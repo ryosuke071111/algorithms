9dc8c56e70295d5d7bd79d19ca6da1b0066f31b2
桑原亮介 <ryosuke0711993@gmail.com>
2019-04-17 21:54:09 +0900

    add unactivated directories2

--- rasen_book/self-solving/largest_square.py
+++
@@ -1,9 +0,0 @@
-H, W = map(int, input().split())
-S = [list(map(int, input().split())) for i in range(H)]
-dp = [[0]*(W) for i in range(H)]
-
-for i in range(H):
-  for j in range(W):
-    if S[i][j] == 0:
-      dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])+1
-print(max([max(a) for a in dp])**2)
